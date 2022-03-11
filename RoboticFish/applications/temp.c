091a90d4c50aee02f69bb780c69381250ecd0166
Tjoms99 <marcus.alex@live.no>
2022-03-11 15:42:09 +0100

    Modified temp.c and temp.h; Added struct to temp; Set up periodic tast for temp_read; Cleaned up useless code

--- RoboticFish/applications/temp.c
+++ RoboticFish/applications/temp.c
@@ -1,22 +1,19 @@
 #include <rtthread.h>
 
+#include "temp.h"
+
 #define THREAD_PRIORITY         25
 #define THREAD_STACK_SIZE       512
 #define THREAD_TIMESLICE        5
 
-static rt_thread_t tid1 = RT_NULL;
+struct sensor_temp sensor_temp = { };
 
 /* Entry Function for Thread 1 */
-static int getTemp(void *parameter)
+static int read_temp(void *parameter)
 {
      /*Read and return temperature*/
     return 0;
 }
-
-ALIGN(RT_ALIGN_SIZE)
-static char storeTemp_stack[1024];
-static struct rt_thread storeTemp;
-
 
 /* Entry for Thread 2 */
 //static void storeTemp(void *param)
@@ -28,27 +25,18 @@ static struct rt_thread storeTemp;
 /* Thread Sample */
 int thread_sample(void)
 {
-    /* Create thread 1, Name is thread1,Entry is thread1_entry */
-//    tid1 = rt_thread_create("getTemp",
-//                            getTemp_entry,
-//                            RT_NULL,
-//                            THREAD_STACK_SIZE,
-//                            THREAD_PRIORITY, THREAD_TIMESLICE);
+   /* Create thread 1, Name is thread1,Entry is thread1_entry */
+    sensor_temp.read_temp = rt_thread_create_periodic("read_temp",              //Name
+                                                       read_temp,               //Function
+                                                       &sensor_temp,            //Stack size; MIGHT NEED TEMP_THREAD_STACH_SIZE INSTEAD
+                                                       READ_TEMP_PRIORITY,      //Priority
+                                                       1,                       //Ticks
+                                                       TEMP_HOST_ACTION_PERIOD  //Period
+                                                       );
 
     /* Start this thread if you get the thread control block */
 //    if (tid1 != RT_NULL)
 //        rt_thread_startup(tid1);
-
-    /* Create thread 2, Name is thread2, Entry is thread2_entry */
-//    rt_thread_init(&storeTemp,
-//                   "storeTemp",
-//                   storeTemp_entry,
-//                   RT_NULL,
-//                   &storeTemp_stack[0],
-//                   sizeof(storeTemp_stack),
-//                   THREAD_PRIORITY - 1, THREAD_TIMESLICE);
-//    rt_thread_startup(&storeTemp);
-
     return 0;
 }
 
--- RoboticFish/applications/temp.h
+++ RoboticFish/applications/temp.h
@@ -7,9 +7,18 @@
  * Date           Author       Notes
  * 2022-03-04     Maja Markusson       the first version
  */
-#ifndef APPLICATIONS_TEMP_H_
-#define APPLICATIONS_TEMP_H_
+#ifndef __APPLICATIONS_TEMP_H__
+#define __APPLICATIONS_TEMP_H__
 
+#define TEMP_THREAD_STACH_SIZE  1024
+#define TEMP_HOST_ACTION_PERIOD 500 /* 5 seconds */
+#define READ_TEMP_PRIORITY 1
+#define STORE_TEMP_PRIORITY 1
 
+struct sensor_temp {
+    rt_thread_t read_temp;
+    rt_thread_t store_temp;
+};
+typedef struct sensor_temp *sensor_temp_t;
 
 #endif /* APPLICATIONS_TEMP_H_ */
