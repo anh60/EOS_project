0bb7aafa000a329868bf78e06076840589cbdbbc
Maja Markusson <majasma@stud.ntnu.no>
2022-03-11 15:43:49 +0100

    changes servo module

--- RoboticFish/applications/servo.c
+++ RoboticFish/applications/servo.c
@@ -15,8 +15,11 @@
  * 2022-03-04     Maja Markusson       the first version
  */
 #include "servo.h"
-#include "rtthread.h"
-//#include <rttime.h>
+#include <rtthread.h>
+
+//init threads
+rt_thread_t servo_thread_set = RT_NULL;
+rt_thread_t servo_thread_calculate = RT_NULL;
 
 //three servomotors could either correspond to a
 //array or they could have a dedicated id
@@ -44,7 +47,7 @@ void servo_init (void) {
      *START TIMER
     */
 
-    //init timers
+    //TIMER HANDLING
     static rt_timer_t servo_timer_set;
     static rt_timer_t servo_timer_calculate;
 
@@ -52,42 +55,39 @@ void servo_init (void) {
     //TODO convert into ms
     int timeout_clockticks = 10;
 
-    servo_timer_set = rt_timer_create("servo_timer_set", servo_calculate_positions,
+    servo_timer_set = rt_timer_create("servo_timer_set", servo_timer_set,
                                  RT_NULL, timeout_clockticks,
                                  RT_TIMER_FLAG_PERIODIC);
 
 
     //TODO the function evoked should be the thread handling the servo
-    servo_timer_calculate = rt_timer_create("servo_timer_calculate", servo_set_positions,
+    servo_timer_calculate = rt_timer_create("servo_timer_calculate", servo_timer_calculate,
                                  RT_NULL, timeout_clockticks,
                                  RT_TIMER_FLAG_PERIODIC);
 
-    //init threads
-//    rt_thread servo_thread_set = RT_NULL;
-//    rt_thread servo_thread_calculate = RT_NULL;
-
+    //THREAD HANDLING
     rt_uint32_t thread_stackSize = 32;
     rt_uint8_t thread_priority = 1;
     rt_uint32_t thread_tick = RT_NULL;
 
-//    servo_thread_set = rt_thread_create("servo_thread_set", servo_set_positions,
-//                                    RT_NULL, thread_stackSize,
-//                                    thread_priority, thread_tick);
+    servo_thread_set = rt_thread_create("servo_thread_set", servo_set_positions,
+                                    RT_NULL, thread_stackSize,
+                                    thread_priority, thread_tick);
 
-//    servo_thread_calculate = rt_thread_create("servo_thread_calculate", servo_set_positions,
-//                                    RT_NULL, thread_stackSize,
-//                                    thread_priority, thread_tick);
+    servo_thread_calculate = rt_thread_create("servo_thread_calculate", servo_set_positions,
+                                    RT_NULL, thread_stackSize,
+                                    thread_priority, thread_tick);
 
-//    if (servo_thread_set == RT_NULL) { return; }
+    if (servo_thread_set == RT_NULL) { return; }
+    if (servo_thread_calculate == RT_NULL) { return; }
 
-//    if (servo_thread_calculate == RT_NULL) { return; }
-
-    //TODO here we need a way to coordinate the first timers so they dont "collide"
-    //start the calculation thread
-//    rt_thread_start(servo_thread_calculate);
 
     //start the timer
-//    if (servo_timer != RT_NULL) rt_timer_start(servo_timer_set);
+    if (servo_timer != RT_NULL) rt_timer_start(servo_timer_calculate);
+
+    if (servo_timer != RT_NULL) rt_timer_start(servo_timer_set);
+
+
 
     return;
 
@@ -99,11 +99,11 @@ void servo_init (void) {
 static void servo_set_positions(int id, int servo_value) {
     //send to device x,y or z
 
-//    rt_err_t err = rt_thread_suspend(servo_thread);
+    rt_err_t err = rt_thread_suspend(servo_thread_set);
 
-//    if (err != RT_EOK) {
+    if (err != RT_EOK) {
         //some error has occurred, fix
-//    }
+    }
 
     rt_schedule();
 
@@ -117,6 +117,4 @@ static int servo_calculate_positions(int servo_value) {
     return servo_value;
 }
 
-/* Export to msh command list */
-//MSH_CMD_EXPORT(thread_sample, thread sample);
 
