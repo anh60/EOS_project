5b5a2f4c87530b9438122d8bdeb2830ced1e4fc2
Tjoms99 <marcus.alex@live.no>
2022-03-12 16:59:35 +0100

    Implemented task scheduling using timers, but does not work

--- RoboticFish/applications/temp.c
+++ RoboticFish/applications/temp.c
@@ -8,6 +8,20 @@ struct sensor_temp sensor_temp;
 rt_timer_t timer_read_temp;
 rt_timer_t timer_store_temp;
 
+
+static void timer_trigger_read_temp(void *param){
+    /*Trigger thread 1*/
+    struct sensor_temp *sensor_temp = param;
+    rt_thread_resume(sensor_temp->read_temp);
+
+}
+
+static void timer_trigger_store_temp(void *param){
+    /*Trigger thread 2*/
+    struct sensor_temp *sensor_temp = param;
+    rt_thread_resume(sensor_temp->store_temp);
+}
+
 /* Thread 1 */
 static void read_temp(void *param)
 {
@@ -19,8 +33,10 @@ static void read_temp(void *param)
     int temp = rand() % range;
 
     sensor_temp->temperature = temp;
-    rt_kprintf("Running read_temp at %d and reading variable %d \n", rt_tick_get(), sensor_temp->temperature);
+    rt_kprintf("Running read_temp at ticks %d and reading variable %d \n", rt_tick_get(), sensor_temp->temperature);
 
+    //rt_thread_yield();
+    //rt_schedule();
 
 }
 
@@ -29,7 +45,10 @@ static void store_temp(void *param)
 {
     /*Store temperature using store_temp*/
     struct sensor_temp *sensor_temp = param;
-    rt_kprintf("Running store_temp at %d and storing variable %d \n", rt_tick_get(), sensor_temp->temperature);
+    rt_kprintf("Running store_temp at ticks %d and storing variable %d \n", rt_tick_get(), sensor_temp->temperature);
+
+    //rt_thread_suspend();
+    //rt_schedule();
 }
 
 /* Initialize temperature sensor */
@@ -38,30 +57,6 @@ sensor_temp_t sensor_temp_init(void)
    /* Initialize variable */
     sensor_temp.temperature = 0;
 
-   /* Initialize timer */
-    timer_read_temp = rt_timer_create("timer_read_temp",
-                               read_temp,
-                               &sensor_temp,
-                               READ_TEMP_ACTION_PERIOD,
-                               (RT_TIMER_FLAG_PERIODIC)
-                               );
-
-    if(!timer_read_temp)
-        return RT_NULL;
-
-    rt_timer_start(timer_read_temp);
-
-    timer_store_temp = rt_timer_create("timer_read_temp",
-                               store_temp,
-                               &sensor_temp,
-                               READ_TEMP_ACTION_PERIOD,
-                               (RT_TIMER_FLAG_PERIODIC)
-                               );
-
-    if(!timer_store_temp)
-        return RT_NULL;
-
-    rt_timer_start(timer_store_temp);
 
    /* Initialize thread 1 */
     sensor_temp.read_temp = rt_thread_create("read_temp",              //Name
@@ -74,6 +69,7 @@ sensor_temp_t sensor_temp_init(void)
 
     if(!sensor_temp.read_temp)
         return RT_NULL;
+    rt_thread_startup(sensor_temp.read_temp);
 
     /* Initialize thread 2 */
      sensor_temp.store_temp = rt_thread_create("store_temp",           //Name
@@ -85,6 +81,32 @@ sensor_temp_t sensor_temp_init(void)
                                                 );
      if(!sensor_temp.store_temp)
          return RT_NULL;
+     rt_thread_startup(sensor_temp.store_temp);
+
+
+     /* Initialize timers */
+      timer_read_temp = rt_timer_create("timer_read_temp",
+                                         read_temp, //Use maybe timer trigger instead
+                                         &sensor_temp,
+                                         READ_TEMP_ACTION_PERIOD,
+                                         RT_TIMER_FLAG_PERIODIC
+                                         );
+
+      if(!timer_read_temp)
+          return RT_NULL;
+      rt_timer_start(timer_read_temp);
+
+
+      timer_store_temp = rt_timer_create("timer_store_temp",
+                                          store_temp,  //Use maybe timer trigger instead
+                                          &sensor_temp,
+                                          READ_TEMP_ACTION_PERIOD,
+                                          RT_TIMER_FLAG_PERIODIC
+                                          );
+
+      if(!timer_store_temp)
+          return RT_NULL;
+      rt_timer_start(timer_store_temp);
 
     return &sensor_temp;
 }
