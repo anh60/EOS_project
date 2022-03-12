786647071e94e721964d25e377cd43bcd4e2cb1b
Maja Markusson <majasma@stud.ntnu.no>
2022-03-12 16:00:50 +0100

    object structure added to servo module and timers back q

--- RoboticFish/applications/servo.c
+++ RoboticFish/applications/servo.c
@@ -23,80 +23,105 @@
 rt_thread_t servo_thread_set = RT_NULL;
 rt_thread_t servo_thread_calculate = RT_NULL;
 
-//three servomotors could either correspond to a
-//array or they could have a dedicated id
 
-//the pseudocode is for spesified id's
+struct servo_motor servo;
 
 void servo_init (void) {
 
     //TIMER HANDLING
-    static rt_timer_t servo_timer_set;
-    static rt_timer_t servo_timer_calculate;
+    //static rt_timer_t servo_timer_init;
 
-    //this is in clockticks
-    //TODO convert into ms
     int timeout_clockticks = 10;
 
-    servo_timer_set = rt_timer_create("servo_timer_set", servo_calculate_positions,
-                                 RT_NULL, timeout_clockticks,
-                                 RT_TIMER_FLAG_PERIODIC);
+    /*servo_timer_init = rt_timer_create("servo_timer_init",
+                                     servo_timer_set,
+                                     RT_NULL,
+                                     timeout_clockticks,
+                                     RT_TIMER_FLAG_PERIODIC);
+    */
+
+    rt_timer_t servo_timer_set = rt_timer_create("servo_timer_set",
+                                      start_thread_1,
+                                      RT_NULL,
+                                      timeout_clockticks,
+                                      (/*RT_TIMER_FLAG_SOFT_TIMER ||*/ RT_TIMER_FLAG_PERIODIC));
 
 
     //TODO the function evoked should be the thread handling the servo
-    servo_timer_calculate = rt_timer_create("servo_timer_calculate", servo_set_positions,
-                                 RT_NULL, timeout_clockticks,
-                                 RT_TIMER_FLAG_PERIODIC);
-
+    rt_timer_t servo_timer_calculate = rt_timer_create("servo_timer_calculate",
+                                            start_thread_2,
+                                            RT_NULL,
+                                            timeout_clockticks,
+                                            (/*RT_TIMER_FLAG_SOFT_TIMER ||*/ RT_TIMER_FLAG_PERIODIC));
 
     //THREAD HANDLING
     rt_uint32_t thread_stackSize = 32;
     rt_uint8_t thread_priority = 1;
     rt_uint32_t thread_tick = RT_NULL;
 
-    servo_thread_set = rt_thread_create("servo_thread_set",
+    servo.servo_thread_set = rt_thread_create("servo_thread_set",
                                         servo_set_positions,
-                                        RT_NULL,
+                                        &servo,
                                         thread_stackSize,
                                         thread_priority,
                                         thread_tick);
 
-    servo_thread_calculate = rt_thread_create("servo_thread_calculate",
-                                               servo_set_positions,
-                                               RT_NULL,
+    servo.servo_thread_calculate = rt_thread_create("servo_thread_calculate",
+                                               servo_calculate_positions,
+                                               &servo,
                                                thread_stackSize,
                                                thread_priority,
                                                thread_tick);
 
-    if (servo_thread_set == RT_NULL) { return; }
-    if (servo_thread_calculate == RT_NULL) { return; }
+
+    if (servo.servo_thread_set == RT_NULL) { return; }
+    if (servo.servo_thread_calculate == RT_NULL) { return; }
+
+    rt_err_t err = rt_timer_start(servo_timer_set);
+    if (err != 0){ return;}
+
+    err = rt_timer_start(servo_timer_calculate);
+    if (err != 0){ return;}
 
     return;
 
 }
 
-//the servo has 10 ms for setting the positions and should
-//do this every 40 ms
+static void servo_set_positions(void *param) {
 
-static void servo_set_positions(int id, int servo_value) {
-    //send to device x,y or z
+    rt_err_t err = rt_thread_suspend(servo.servo_thread_set);
 
-    rt_err_t err = rt_thread_suspend(servo_thread_set);
-
-    if (err != RT_EOK) {
-        //some error has occurred, fix
-    }
+    if (err != RT_EOK) { /*some error has occurred, fix*/ }
 
     rt_schedule();
 
     return;
 }
 
-static int servo_calculate_positions(int servo_value) {
-    //some math non lo so
-    servo_value = servo_value + 1;
+static void servo_calculate_positions(void *param) {
+    struct servo_motor *servo = param;
 
-    return servo_value;
+    servo->servo_value[0] = 0;
+    servo->servo_value[1] = 1;
+    servo->servo_value[2] = 2;
+
+
+    rt_err_t err = rt_thread_suspend(servo->servo_thread_calculate);
+
+    if (err != RT_EOK) { /*some error has occurred, fix*/ }
+
+    rt_schedule();
+
+}
+
+static void start_thread_1(void *param){
+    rt_thread_startup(servo.servo_thread_set);
+    return;
+}
+
+static void start_thread_2(void *param){
+    rt_thread_startup(servo.servo_thread_calculate);
+    return;
 }
 
 
--- RoboticFish/applications/servo.h
+++ RoboticFish/applications/servo.h
@@ -7,13 +7,27 @@
  * Date           Author       Notes
  * 2022-03-04     Maja Markusson       the first version
  */
+#include <rtdef.h>
+
 #ifndef APPLICATIONS_SERVO_H_
 #define APPLICATIONS_SERVO_H_
 
-#define RT_TIMER_FLAG_PERIODIC      0x2     /* Periodic timing     */
+#define RT_TIMER_FLAG_PERIODIC      0x2     /* Periodic timing */
+#define RT_TIMER_FLAG_SOFT_TIMER    0x4     /* Software timer  */
+#define HOST_ACTION_PERIOD          40
+
+struct servo_motor {
+    rt_thread_t servo_thread_calculate;
+    rt_thread_t servo_thread_set;
+
+    int servo_value[3];
+};
+
+typedef struct servo_motor *servo_motor_p;
 
 void servo_init(void);
-static void servo_set_positions(int id, int servo_value);
-static int servo_calculate_positions(int servo_value);
-
+static void servo_set_positions(void *param);
+static void servo_calculate_positions(void *param);
+static void start_thread_1(void *param);
+static void start_thread_2(void *param);
 #endif /* APPLICATIONS_SERVO_H_ */
